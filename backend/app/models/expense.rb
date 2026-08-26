636c78f6d86f61ff59301a7bd2800e27623b4d35
BeamRaileyDamian <125461358+BeamRaileyDamian@users.noreply.github.com>
2026-08-27 01:23:41 +0800

    fix: disallow future dates in BE

--- backend/app/models/expense.rb
+++ backend/app/models/expense.rb
@@ -1,3 +1,16 @@
 class Expense < ApplicationRecord
   belongs_to :category
+
+  validates :date, presence: true
+  validate :date_not_in_future
+
+  private
+
+  def date_not_in_future
+    return if date.blank?
+
+    if date > Date.current
+      errors.add(:date, "can't be in the future")
+    end
+  end
 end
